29fb2532abc561b268064d63ca050d9f168d67ab
raulcampost <ad712ecb00025416>
2018-06-05 13:13:09 -0600

    abstract factory recursos

---
+++ recursos/AbstractFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author UCA
+ */
+public class AbstractFactory {
+    Recurso1 GetRecurso1(String type);
+    Recurso2 GetRecurso2(String type);
+    Recurso3 GetRecurso3(String type);
+}
